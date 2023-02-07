dc7227bf6f86ebd50be7b91efaf33827c42dbcba
JoseHurtado03 <jose.hurtado@correo.unimet.edu.ve>
2023-02-07 18:02:23 -0400

    :rocket: Jose

---
+++ Proyecto-1-moisesliota/Proyecto_1/src/proyecto_1/Vertice.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_1;
+
+/**
+ *
+ * @author moise
+ */
+public class Vertice {
+    //Atributo de la clase vertice
+    private Object dato;
+    
+    //Constructor de la clase Vertice
+
+    public Vertice(Object dato) {
+        this.dato = dato;
+    }
+   
+    //Getter and Setter
+    public Object getDato() {
+        return dato;
+    }
+
+    public void setDato(Object dato) {
+        this.dato = dato;
+    }
+    
+    
+}
---
+++ Proyecto-1-moisesliota/Proyecto_1/src/proyecto_1/main.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_1;
+
+/**
+ *
+ * @author moise
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Grafo grafo1 = new Grafo(10, true);
+        grafo1.insertarVertice(1);
+        
+        
+//        int num = 1;
+//        int num_repro = 1003443;
+//        int age = 1992;
+//        String name = "Hallelujah";
+//        String artist = "Lonard Cohen";
+//        String idioma = "Español";
+//        
+//        Cancion song1 = new Cancion(num, name, artist, num_repro, age, idioma);
+//        
+//        grafo1.insertarArista(1, song1);
+//        
+//        grafo1.imprimirGrafo();
+        
+    }
+    
+}
