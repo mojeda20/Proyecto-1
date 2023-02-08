07da5d6ea2e3a3b27609a8c7303224f1c36fde91
JoseHurtado03 <jose.hurtado@correo.unimet.edu.ve>
2023-02-07 22:09:43 -0400

    :memo: Base Dijkstra

---
+++ Proyecto 1 - José/src/proyecto1/Dijkstra.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+
+public class Dijkstra {
+    private int [][] distances;
+    private int [] last;
+    private int [] shorter_d;
+    private boolean [] F;
+    private int origin, num_ver;
+    
+//    public void ShorterPath(int origin /*Grafo grafo*/){
+//        num_ver = grafo.NumeroDeVertices();
+//        this.origin = origin;
+//        distances = grafo.Distances();
+//        last = new int[num_ver];
+//        F = new boolean[num_ver];
+//        
+//        for (int i = 0; i < num_ver; i++){
+//            F[i] = false;
+//            shorter_d[i] = distances[origin][i];
+//            last[i] = origin;
+//        }
+//        F[origin] = true;
+//        shorter_d[origin] = 0;
+//        for (int i=1; i < num_ver; i++){
+//            
+//        }
+//    }
+//    private int minimo()
+}
